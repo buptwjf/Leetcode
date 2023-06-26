7d4f9206f187b716f91287aa1d2ca02c6f39d392
jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>
2023-06-26 22:52:51 +0800

    add offer2 code
    Signed-off-by: jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>

--- 0_offer2/string/58.cpp
+++ 0_offer2/string/58.cpp
@@ -14,8 +14,8 @@ public:
         return res;
     }*/
     string reverseLeftWords(string s, int n) {
-        reverse(s.begin(), s.begin()+2);
-        reverse(s.begin()+2, s.end());
+        reverse(s.begin(), s.begin()+n);
+        reverse(s.begin()+n, s.end());
         reverse(s.begin(), s.end());
         return s;
     }
--- 0_offer2/two_pointer/52.cpp
+++ 0_offer2/two_pointer/52.cpp
@@ -2,3 +2,56 @@
 // Created by 86188 on 2023/6/25.
 //
 // 两个链表的第一个公共节点
+
+
+/*
+ * 思路：
+ * 1. 统计两个链表的长度
+ * 2. 根据长度差采用双指针
+ * 3. 指针连续后移
+ *
+ * 时间 O(n)， 空间 O(1)
+ * */
+
+#include "../include/user.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int len_A = 0;
+        int len_B = 0;
+        // 必须有两个临时的指针代表 headA headB
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        while (curA) {
+            curA = curA->next;
+            len_A++;
+        }
+        while (curB) {
+            curB = curB->next;
+            len_B++;
+        }
+        int diff = abs(len_A - len_B);
+        if (len_A > len_B) {
+            while (diff--) {
+                headA = headA->next;
+            }
+        } else {
+            while (diff--) {
+                headB = headB->next;
+            }
+        }
+
+        // 同时后移
+        while (headA && headB) {
+            if (headA == headB) {
+                break;
+            }
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return headA;
+    }
+};
