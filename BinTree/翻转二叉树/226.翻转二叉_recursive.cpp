41a45cc796bbdcc77bab049b81b6c869bb5ababa
jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>
2023-12-08 15:33:11 +0800

    BinTree Note.md
    Signed-off-by: jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>

---
+++ BinTree/翻转二叉树/226.翻转二叉_recursive.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+/*
+    翻转二叉树的思路, 采用递归的方式，对于单独一个节点来说，先交换他的左右孩子，然后对左右节点作为根进行翻转
+*/
+
+// 采用后续遍历的递归法
+class Solution {
+  public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (root) {
+            TreeNode *cur = root;
+            invertTree(cur->left);
+            invertTree(cur->right);
+            swap(cur->left, cur->right);
+            return root;
+        }
+        return root;
+    }
+};
+
+// 采用先序遍历的递归法
+class Solution1 {
+  public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (root) {
+            TreeNode *cur = root;
+            swap(cur->left, cur->right);
+            invertTree(cur->left);
+            invertTree(cur->right);
+            return root;
+        }
+        return root;
+    }
+};
+// @lc code=end
---
+++ BinTree/翻转二叉树/226.翻转二叉树_Traversal.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ */
+
+#include "../../0_offer2/include/user.h"
+
+using namespace std;
+// @lc code=start
+
+
+/*
+    翻转二叉树的思路: 采用 traversal写法，前序或后序遍历，同时交换左右节点
+*/
+
+// 采用后续遍历的递归法
+class Solution {
+
+public:
+    void traversal(TreeNode *root) {
+        if (!root) {
+            return;
+        }
+        TreeNode *temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        traversal(root->left);
+        traversal(root->right);
+    }
+
+    TreeNode *invertTree(TreeNode *root) {
+        traversal(root);
+        return root;
+    }
+};
+
+
+
+// @lc code=end
